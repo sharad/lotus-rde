2ae27f56897862318a3eebae252e926e11a42748
Sharad Pratap <110339+sharad@users.noreply.github.com>
2025-12-01 17:25:55 +0530

    correction

---
+++ src/lotus-rde/rde/api/defaults.scm
@@ -0,0 +1,157 @@
+(define-module (lotus-rde rde api defaults)
+  ;; #:use-module (rde features)
+  ;; #:use-module (rde features emacs)
+  ;; #:use-module (rde predicates)
+  ;; #:use-module (rde serializers elisp)
+  ;; #:use-module (gnu services)
+  ;; #:use-module (gnu home services)
+  ;; #:use-module (gnu packages markup)
+  ;; #:use-module (gnu packages haskell-xyz)
+  ;; #:use-module (gnu packages emacs-xyz)
+  ;; #:use-module (gnu packages tex)
+  #:use-module (guix gexp)
+  #:use-module (guix build utils)
+  #:use-module (lotus-rde rde api misc)
+
+  #:export (build-mapped-device))
+
+
+
+
+
+
+
+
+(define build-srv-local-file-system  (build-parent-dir-file-system-builder "/srv/volumes/local"))
+(define build-srv-distro-file-system (build-parent-dir-file-system-builder "/srv/volumes/distro"))
+
+
+(define %local-udev-lvm-mapped-devices             '())
+(define %local-mount-lvm-non-system-file-systems   '())
+(define %local-unmount-lvm-non-system-file-systems '())
+
+
+(define %local-fs-boot-efi-partition (uuid "0000-0000" 'fat32))
+(define %local-fs-boot-efi-device    "/dev/sda1")
+
+
+(define %lotus-local-services (list (service tor-service-type)))
+
+
+(define %local-grub-ubuntu-menuentries (list))
+
+(define %lotus-grub-bootloader grub-efi-bootloader)
+(define %lotus-grub-targets    '("/boot/efi"))
+
+
+(define %local-bitlbee-service-use-default?  #f)
+(define %local-account-uid                   1000)
+(define %local-account-user-name             "s")
+(define %local-account-comment               "sharad")
+(define %local-account-group-name            "users")
+(define %local-account-group-gid              1000)
+(define %local-account-supplementry-groups   (append '("wheel" "netdev" "audio" "video")
+                                                     (if %lotus-system-init
+                                                       '()
+                                                       '("docker"))))
+(define %local-account-home-parent-directory "/home")
+(define %local-account-relative-home-directory (string-append %local-account-user-name "/" "hell"))
+(define %local-account-shell                 #~(string-append #$zsh "/bin/zsh"))
+
+
+(define %local-guest-user-name             "guest")
+(define %local-guest-comment               "Guest")
+(define %local-guest-group-name            "users")
+(define %local-guest-group-gid             %local-account-group-name)
+(define %local-guest-relative-home-directory %local-guest-user-name)
+(define %local-guest-supplementry-groups   (append '("netdev" "audio" "video")
+                                                   (if %lotus-system-init
+                                                     '()
+                                                     '("docker"))))
+
+
+(define %local-gdm-auto-login                #f)
+(define %local-gdm-allow-empty-password      #t)
+
+
+
+
+;; (define %local-nm-dnsmasq-ns-path         #~(string-append #$nm-dnsmasq-ns "/etc/NetworkManager/dnsmasq.d"))
+
+(define %local-account-create-home-directory #f)
+(define %local-network-manager-dns           "dnsmasq")
+
+
+(define %local-guix-publish-advertise          #t)
+(define %local-guix-publish-port               80)
+(define %local-guix-publish-host               "0.0.0.0")
+(define %local-guix-publish-compression        '(("lzip" 7) ("gzip" 9)))
+(define %local-guix-publish-cache              "/var/cache/guix/publish")
+(define %local-guix-publish-cache-bypass-threshold (* 100 1024 1024))
+(define %local-guix-publish-ttl                (* 1 24 60 60))
+
+
+(define %local-guix-configuration-discover       #t)
+(define %local-guix-configuration-build-accounts 10)
+(define %local-guix-configuration-authorize-key? #f)
+(define %local-guix-configuration-local-fixed-named-substitute-urls '())
+;; "http://local.guix-01"
+;; "http://local.guix-02"
+;; "http://local.guix-03"
+;; "http://local.guix-04"
+
+(define %local-guix-configuration-local-substitute-urls '())
+
+(define %local-guix-configuration-substitute-urls       '(;; "https://ci.guix.gnu.org"
+                                                          ;; "https://bayfront.guixsd.org"
+                                                          ;; "http://guix.genenetwork.org" -- Backtrace
+                                                          ;; "https://berlin.guixsd.org"
+                                                          "https://cuirass.genenetwork.org"
+                                                          "https://guix.tobias.gr"
+                                                          "https://bordeaux.guix.gnu.org"
+                                                          "https://ci.guix.info/"
+                                                          "https://berlin.guix.gnu.org"
+                                                          "https://substitutes.nonguix.org"))
+(define %local-guix-configuration-extra-options         '(
+                                                          ;; "--max-jobs=2"
+                                                          ;; "--cores=1"
+                                                          "--gc-keep-derivations=yes"
+                                                          "--gc-keep-outputs=yes"))
+(define %local-guix-configuration-use-substitutes       #t) ;always true
+;; (define %local-guix-configuration-tmpdir         #f)
+(define %local-guix-configuration-tmpdir "/tmp/guix-build-workspace/build/tmp")
+
+
+(define %local-fs-guix-check?                   #t)
+(define %local-fs-guix-root-check?              %local-fs-guix-check?)
+(define %local-fs-guix-boot-check?              %local-fs-guix-check?)
+(define %local-fs-guix-gnu-check?               %local-fs-guix-check?)
+(define %local-fs-sys-tmp-check?                %local-fs-guix-check?)
+(define %local-fs-guix-var-check?               %local-fs-guix-check?)
+
+
+(define %local-fs-house-check?                  #t)
+(define %local-fs-house-home-check?             %local-fs-house-check?)
+
+(define %local-guix-boot-mount?                 #f)
+(define %local-guix-boot-create-mount-point?    #f)
+(define %local-guix-boot-needed-for-boot?       #f)
+(define %local-guix-bootefi-mount?              #f)
+(define %local-guix-bootefi-create-mount-point? #f)
+(define %local-guix-bootefi-needed-for-boot?    #f)
+
+
+(define %local-locale "en_US.utf8")
+
+(define %local-locate-names (list "en_US"
+                                  "hi_IN"
+                                  "ur_PK"
+                                  "fa_IR"
+                                  "ar_SA"))
+
+
+(define %local-timezone  "Asia/Kolkata")
+
+
+(define %local-kernel-loadable-modules (list v4l2loopback-linux-module))
+
---
+++ src/lotus-rde/rde/api/misc.scm
@@ -0,0 +1,266 @@
+(define-module (lotus-rde rde api misc)
+  ;; #:use-module (rde features)
+  ;; #:use-module (rde features emacs)
+  ;; #:use-module (rde predicates)
+  ;; #:use-module (rde serializers elisp)
+  ;; #:use-module (gnu services)
+  ;; #:use-module (gnu home services)
+  ;; #:use-module (gnu packages markup)
+  ;; #:use-module (gnu packages haskell-xyz)
+  ;; #:use-module (gnu packages emacs-xyz)
+  ;; #:use-module (gnu packages tex)
+  #:use-module (lotus-rde rde api defaults)
+  #:use-module (guix gexp)
+  #:use-module (guix build utils)
+
+  #:export (build-mapped-device
+            build-parent-dir-file-system-builder))
+
+
+
+
+(define %local-host-name "komputilo")
+(define %local-default-realm  '((#:default #f)
+                                (#:realms)))
+
+
+(define %local-disk-serial-id          "CHANGEIT")
+(define %local-disk-prefix             "vds")
+(define %local-disk-suffix-seq         01)
+(define %local-fs-check? #t)
+
+(define (build-local-disk-serial-id)
+  %local-disk-serial-id)
+
+
+
+
+;; http://www.shido.info/lisp/scheme_syntax_e.html
+(define-syntax base-system-value
+  (syntax-rules ()
+    ((_ local alternate)
+     (if (not %lotus-system-init) local alternate))))
+
+(define-syntax nongnu-system-value
+  (syntax-rules ()
+    ((_ local alternat)
+     (if (and %lotus-use-nongnu
+              (not %lotus-system-init))
+         local alternat))))
+
+
+
+;; Functions
+(define (local-authorized-guix-keys-local-to-etc-config-file etc-subdir local-dir)
+  (let ((currdir (string-append (dirname (current-filename)) "/")))
+    (if (file-exists? (string-append currdir local-dir "/"))
+        (map (lambda (f)
+               (list (string-append etc-subdir "/" (substring f (string-length currdir)))
+                     (local-file f)))
+             (find-files (string-append currdir local-dir "/")
+                         ".pub"))
+        '())))
+
+(define (local-authorized-guix-keys dir)
+  (if (file-exists? dir)
+      (map (lambda (f) (local-file f))
+           (find-files (string-append dir "/")
+                       ".pub"))
+      '()))
+
+
+
+
+(define (open-lvm-device source targets)
+  ;; BUG: Fix using https://github.com/Webconverger/webc/blob/1164d83512305d49a8c3dc37b5d26e2d4dd84204/usr/share/initramfs-tools/scripts/init-top/udev
+  "Return a gexp that maps SOURCES to TARGETSS as a LVM device, using
+'lvm'."
+  (with-imported-modules (source-module-closure '((gnu build file-systems)))
+                         #~(let ((source   #$source)
+                                 (targets  '#$targets)
+                                 (lvm-bin  #$(file-append lvm2-static "/sbin/lvm")))
+                             ;; Use 'lvm2-static', not 'lvm2', to avoid pulling the
+                             ;; whole world inside the initrd (for when we're in an initrd).
+                             (begin
+                               (format #t "Enabling ~a~%" targets)
+                               (map (lambda (trg)
+                                      (let ((trg-mapper-path (string-append "/dev/mapper/" trg)))
+                                        (unless (file-exists? trg-mapper-path)
+                                          (zero? (system* lvm-bin "vgscan" "--mknodes"))
+                                          (unless (file-exists? trg-mapper-path)
+                                            (sleep 1)
+                                            (zero? (system* lvm-bin "vgchange" "-ay" (car (string-split trg #\-))))
+                                            (unless (file-exists? trg-mapper-path)
+                                              (sleep 1)
+                                              (zero? (system* lvm-bin "lvchange" "-aay" "-y" "--sysinit" "--ignoreskippedcluster"
+                                                              (string-join (string-split trg #\-) "/")))
+                                              (unless (file-exists? trg-mapper-path)
+                                                (sleep 1)
+                                                (zero? (system* lvm-bin "vgmknodes" "--refresh" (car (string-split trg #\-))))))))))
+                                    targets)
+                               #t))))
+
+(define (close-lvm-device source targets)
+  "Return a gexp that closes TARGETS, a LVM device."
+  #~(let ((source   #$source)
+          (targets  '#$targets)
+          (lvm-bin  #$(file-append lvm2-static "/sbin/lvm")))
+      (format #t "Disabling ~a~%" targets)
+      (begin
+        (map (lambda (trg)
+               (let ((trg-mapper-path (string-append "/dev/mapper/" trg)))
+                 (when (file-exists? trg-mapper-path)
+                   (zero? (system* lvm-bin "lvchange" "-an" "-y" (string-join (string-split trg #\-) "/"))))))
+             targets)
+        #t)))
+
+;; The type of LVM mapped devices.
+(define lvm-device-mapping (mapped-device-kind (open open-lvm-device)
+                                               ;; (check check-lvm-device)
+                                               (close close-lvm-device)))
+
+
+(define* (lotus-lvm-dev-fs-builders serial-id #:key (prefix "vds") (suffix-seq 01) (separator "X"))
+
+  ;; (display "serial-id ~a~%" serial-id)
+
+  (define (get-string s)
+    (cond ((string? s) s)
+          ((procedure? s)
+           (s))
+          (else (error (format #t "Wrong value ~a~%" s)))))
+
+  (define (get-number n)
+    (cond ((number? n) n)
+          ((procedure? n)
+           (n))
+          (else (error (format #t "Wrong value ~a~%" n)))))
+
+  (define (get-serial-id serial-id prefix)
+    (string-append (get-string prefix)
+                   (get-string serial-id)))
+
+  (define (get-group group suffix-seq)
+    (let ((suffix-seq (get-number suffix-seq)))
+      (string-append group
+                     (if (> suffix-seq 0)
+                         (format #f "~2'0d" suffix-seq)
+                         ""))))
+  (define* (dev-builder group volume #:key (suffix-seq suffix-seq))
+    (mapped-device (source (string-append (get-serial-id serial-id
+                                                         prefix)
+                                          "X"
+                                          (get-group group
+                                                     suffix-seq)))
+                   (targets (list (string-append (get-serial-id serial-id
+                                                                prefix)
+                                                 "X"
+                                                 (string-join (list (get-group group
+                                                                               suffix-seq)
+                                                                    volume)
+                                                              "-"))))
+                   (type   lvm-device-mapping)))
+
+  (define* (fs-builder mount-point
+                              group
+                              volume
+                              #:key
+                              (suffix-seq suffix-seq)
+                              (type "ext4")
+                              (check? #t)
+                              (mount? #t)
+                              (flags  '())
+                              (options #f)
+                              (create-mount-point? #t)
+                              (needed-for-boot?    #t)
+                              (dependencies (list)))
+           (file-system (mount-point         mount-point)
+                        (device              (string-append "/dev/mapper/"
+                                                            (get-serial-id serial-id
+                                                                           prefix)
+                                                            "X" (string-join (list (get-group group
+                                                                                              suffix-seq)
+                                                                                   volume)
+                                                                             "-")))
+                        (type                type)
+                        (check?              check?)
+                        (mount?              mount?)
+                        (flags               flags)
+                        (options             options)
+                        (create-mount-point? create-mount-point?)
+                        (needed-for-boot?    needed-for-boot?)
+                        (dependencies        dependencies)))
+
+  (define (get-disk-name) (get-serial-id serial-id prefix))
+
+  (values dev-builder
+          fs-builder
+          get-disk-name))
+
+
+;; from local-default.scm
+
+(define (build-mapped-device source disk-id vg lv)
+  (mapped-device (source source)
+                 (targets (list (string-append disk-id "X" (string-append vg "-" lv))))
+                 (type   lvm-device-mapping)))
+
+
+
+(define-values (lotus-build-mapped-device lotus-build-file-system lotus-get-disk-name)
+  (lotus-lvm-dev-fs-builders (lambda () %lotus-disk-serial-id)
+                             #:prefix (lambda () %lotus-disk-prefix)
+                             #:suffix-seq (lambda () %lotus-disk-suffix-seq)))
+
+(define-values (lotus-local-build-mapped-device lotus-local-build-file-system local-lotus-get-disk-name)
+  (lotus-lvm-dev-fs-builders (lambda () %local-disk-serial-id)
+                             #:prefix (lambda () %local-disk-prefix)
+                             #:suffix-seq 0))
+
+
+(define %local-home-build-mapped-device lotus-build-mapped-device)
+(define %local-home-build-file-system   lotus-build-file-system)
+
+
+(define* (build-parent-dir-file-system-builder parent-dir
+                                              #:key
+                                              (dep-mapped-devs (lambda () %local-udev-lvm-mapped-devices))
+                                              (builder         lotus-local-build-file-system)
+                                              (disk-name       local-lotus-get-disk-name)
+                                              (check?          %local-fs-check?)
+                                              (mount?          #f)
+                                              (flags           '())
+                                              (options         #f)
+                                              (type            "ext4")
+                                              (create-mount-point? #f)
+                                              (needed-for-boot? #f))
+  (define* (fs-builder vg lv
+                       #:key
+                       (dep-mapped-devs dep-mapped-devs)
+                       (builder         builder)
+                       (disk-name       disk-name)
+                       (check?          check?)
+                       (mount?          mount?)
+                       (flags           flags)
+                       (options         options)
+                       (type            type)
+                       (create-mount-point? create-mount-point?)
+                       (needed-for-boot? needed-for-boot?))
+    (define (get-value v)
+      (cond ((procedure? v)
+             (v))
+            (else v)))
+    (builder (string-append parent-dir "/" (disk-name) "/" vg "/" lv)
+             vg
+             lv
+             #:check?  check?
+             #:mount?  mount?
+             #:flags   flags
+             #:options options
+             #:type    type
+             #:create-mount-point? create-mount-point?
+             #:needed-for-boot?    needed-for-boot?
+             #:dependencies        (get-value dep-mapped-devs)))
+
+  fs-builder)
+
