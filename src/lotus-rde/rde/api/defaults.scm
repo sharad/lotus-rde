8c49e65d05f3bb3f53f9a243db47caf44b8e07aa
Sharad Pratap <110339+sharad@users.noreply.github.com>
2025-12-04 14:13:20 +0530

    correction

--- src/lotus-rde/rde/api/defaults.scm
+++ src/lotus-rde/rde/api/defaults.scm
@@ -17,7 +17,7 @@
   #:use-module (gnu bootloader grub)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages shells)
-  #:use-module (lotus-rde rde api misc)
+  #:use-module (lotus-rde rde api utils)
   #:export (build-mapped-device))
 
 
