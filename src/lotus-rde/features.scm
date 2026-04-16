218ec232a599df5dc4639ebc45507b65e879592c
Sharad Pratap <110339+sharad@users.noreply.github.com>
2026-04-16 15:10:34 +0530

    correction

--- src/lotus-rde/features.scm
+++ src/lotus-rde/features.scm
@@ -1,142 +1,143 @@
 
 (define-module (lotus-rde features)
-  #:export ())
+  #:use-module (guix records)
+  #:export (lotus-get-operating-system))
 
-;; (define (lotus-get-operating-system config)
-;;   (when (rde-config-integrate-he-in-os? config)
-;;     (require-value 'user-name config))
-;;   (let* ((initial-os (rde-config-initial-os config))
+(define (lotus-get-operating-system config)
+  (when (rde-config-integrate-he-in-os? config)
+    (require-value 'user-name config))
+  (let* ((initial-os (rde-config-initial-os config))
 
-;;          (rde-host-name    (get-value
-;;                             'host-name config
-;;                             (operating-system-host-name initial-os)))
-;;          (rde-timezone     (get-value
-;;                             'timezone config
-;;                             (operating-system-timezone initial-os)))
-;;          (rde-locale       (get-value
-;;                             'locale config
-;;                             (operating-system-locale initial-os)))
-;;          (rde-issue        (get-value
-;;                             'issue config
-;;                             (operating-system-locale initial-os)))
-;;          (rde-keyboard-layout
-;;                            (get-value
-;;                             'keyboard-layout config
-;;                             (operating-system-keyboard-layout initial-os)))
-;;          (rde-bootloader-cfg
-;;                            (get-value
-;;                             'bootloader-configuration config
-;;                             (operating-system-bootloader initial-os)))
-;;          (rde-bootloader   (bootloader-configuration
-;;                             (inherit rde-bootloader-cfg)
-;;                             (keyboard-layout rde-keyboard-layout)))
-;;          (rde-mapped-devices
-;;                            (get-value
-;;                             'mapped-devices config
-;;                             (operating-system-mapped-devices initial-os)))
-;;          (rde-swap-devices (get-value
-;;                             'swap-devices config
-;;                             (operating-system-swap-devices initial-os)))
-;;          (rde-file-systems (get-value
-;;                             'file-systems config
-;;                             (operating-system-file-systems initial-os)))
+         (rde-host-name    (get-value
+                            'host-name config
+                            (operating-system-host-name initial-os)))
+         (rde-timezone     (get-value
+                            'timezone config
+                            (operating-system-timezone initial-os)))
+         (rde-locale       (get-value
+                            'locale config
+                            (operating-system-locale initial-os)))
+         (rde-issue        (get-value
+                            'issue config
+                            (operating-system-locale initial-os)))
+         (rde-keyboard-layout
+                           (get-value
+                            'keyboard-layout config
+                            (operating-system-keyboard-layout initial-os)))
+         (rde-bootloader-cfg
+                           (get-value
+                            'bootloader-configuration config
+                            (operating-system-bootloader initial-os)))
+         (rde-bootloader   (bootloader-configuration
+                            (inherit rde-bootloader-cfg)
+                            (keyboard-layout rde-keyboard-layout)))
+         (rde-mapped-devices
+                           (get-value
+                            'mapped-devices config
+                            (operating-system-mapped-devices initial-os)))
+         (rde-swap-devices (get-value
+                            'swap-devices config
+                            (operating-system-swap-devices initial-os)))
+         (rde-file-systems (get-value
+                            'file-systems config
+                            (operating-system-file-systems initial-os)))
 
-;;          (rde-user-name    (get-value 'user-name config #f))
-;;          (rde-user-id      (get-value 'user-id config 1000))
-;;          (rde-user-group   (get-value 'user-group config "users"))
-;;          (rde-full-name    (get-value 'full-name config ""))
-;;          (rde-user-groups  (get-value 'user-groups config '()))
-;;          (rde-home-directory
-;;                            (get-value
-;;                             'home-directory config
-;;                             (string-append "/home/" (or rde-user-name "user"))))
-;;          (rde-login-shell  (get-value 'login-shell config (default-shell)))
-;;          (rde-user-password
-;;                            (get-value 'user-initial-password-hash config #f))
+         (rde-user-name    (get-value 'user-name config #f))
+         (rde-user-id      (get-value 'user-id config 1000))
+         (rde-user-group   (get-value 'user-group config "users"))
+         (rde-full-name    (get-value 'full-name config ""))
+         (rde-user-groups  (get-value 'user-groups config '()))
+         (rde-home-directory
+                           (get-value
+                            'home-directory config
+                            (string-append "/home/" (or rde-user-name "user"))))
+         (rde-login-shell  (get-value 'login-shell config (default-shell)))
+         (rde-user-password
+                           (get-value 'user-initial-password-hash config #f))
 
-;;          (rde-user         (if rde-user-name
-;;                                (user-account
-;;                                 (uid  rde-user-id)
-;;                                 (name rde-user-name)
-;;                                 (comment rde-full-name)
-;;                                 (password rde-user-password)
-;;                                 (home-directory rde-home-directory)
-;;                                 (shell rde-login-shell)
-;;                                 (group rde-user-group)
-;;                                 (supplementary-groups rde-user-groups))
-;;                                #f))
+         (rde-user         (if rde-user-name
+                               (user-account
+                                (uid  rde-user-id)
+                                (name rde-user-name)
+                                (comment rde-full-name)
+                                (password rde-user-password)
+                                (home-directory rde-home-directory)
+                                (shell rde-login-shell)
+                                (group rde-user-group)
+                                (supplementary-groups rde-user-groups))
+                               #f))
 
-;;          (rde-services     (rde-config-system-services config))
+         (rde-services     (rde-config-system-services config))
 
-;;          (rde-kernel       (get-value
-;;                             'kernel config
-;;                             (operating-system-kernel initial-os)))
-;;          (rde-kernel-arguments
-;;                            (get-value
-;;                             'kernel-arguments config
-;;                             (operating-system-user-kernel-arguments initial-os)))
-;;          (rde-kernel-modules
-;;                            (get-value
-;;                             'kernel-loadable-modules config
-;;                             (operating-system-kernel-loadable-modules initial-os)))
-;;          (rde-initrd       (get-value
-;;                             'initrd config
-;;                             (operating-system-initrd initial-os)))
-;;          (rde-initrd-modules
-;;                            (get-value
-;;                             'initrd-modules config
-;;                             (operating-system-initrd-modules initial-os)))
-;;          (rde-firmware     (get-value
-;;                             'firmware config
-;;                             (operating-system-firmware initial-os)))
-;;          (rde-name-service-switch
-;;                            (get-value
-;;                             'name-service config
-;;                             %default-nss))
+         (rde-kernel       (get-value
+                            'kernel config
+                            (operating-system-kernel initial-os)))
+         (rde-kernel-arguments
+                           (get-value
+                            'kernel-arguments config
+                            (operating-system-user-kernel-arguments initial-os)))
+         (rde-kernel-modules
+                           (get-value
+                            'kernel-loadable-modules config
+                            (operating-system-kernel-loadable-modules initial-os)))
+         (rde-initrd       (get-value
+                            'initrd config
+                            (operating-system-initrd initial-os)))
+         (rde-initrd-modules
+                           (get-value
+                            'initrd-modules config
+                            (operating-system-initrd-modules initial-os)))
+         (rde-firmware     (get-value
+                            'firmware config
+                            (operating-system-firmware initial-os)))
+         (rde-name-service-switch
+                           (get-value
+                            'name-service config
+                            %default-nss))
 
-;;          (computed-os
-;;           (operating-system
-;;             (inherit initial-os)
-;;             (host-name rde-host-name)
-;;             (timezone rde-timezone)
-;;             (locale rde-locale)
-;;             (issue rde-issue)
-;;             (bootloader rde-bootloader)
-;;             (mapped-devices rde-mapped-devices)
-;;             (swap-devices rde-swap-devices)
-;;             (file-systems rde-file-systems)
-;;             (keyboard-layout rde-keyboard-layout)
-;;             (kernel rde-kernel)
-;;             (kernel-arguments rde-kernel-arguments)
-;;             (kernel-loadable-modules rde-kernel-modules)
-;;             (initrd rde-initrd)
-;;             (initrd-modules rde-initrd-modules)
-;;             (firmware rde-firmware)
-;;             (services (append
-;;                        rde-services
-;;                        (if (rde-config-integrate-he-in-os? config)
-;;                            (list (service guix-home-service-type
-;;                                           ;; TODO: [Andrew Tropin, 2024-05-27]
-;;                                           ;; Temporary fix, remove it, when
-;;                                           ;; https://issues.guix.gnu.org/71111 is
-;;                                           ;; merged
-;;                                           `(,(list
-;;                                               rde-user-name
-;;                                               (get-home-environment config)))))
-;;                            '())
-;;                        (list (service sudoers-service-type))
-;;                        (if rde-user-name
-;;                            (list (service rde-account-service-type rde-user))
-;;                            '())))
-;;             (sudoers-file #f)
-;;             (name-service-switch rde-name-service-switch))))
-;;     ;; Only apply transformations on thunked fields here.
-;;     (operating-system
-;;       (inherit computed-os)
-;;       (essential-services
-;;        (modify-services (operating-system-essential-services computed-os)
-;;          (shepherd-root-service-type
-;;           this-config =>
-;;           (shepherd-configuration
-;;            (inherit this-config)
-;;            (shepherd (get-value 'shepherd config)))))))))
+         (computed-os
+          (operating-system
+            (inherit initial-os)
+            (host-name rde-host-name)
+            (timezone rde-timezone)
+            (locale rde-locale)
+            (issue rde-issue)
+            (bootloader rde-bootloader)
+            (mapped-devices rde-mapped-devices)
+            (swap-devices rde-swap-devices)
+            (file-systems rde-file-systems)
+            (keyboard-layout rde-keyboard-layout)
+            (kernel rde-kernel)
+            (kernel-arguments rde-kernel-arguments)
+            (kernel-loadable-modules rde-kernel-modules)
+            (initrd rde-initrd)
+            (initrd-modules rde-initrd-modules)
+            (firmware rde-firmware)
+            (services (append
+                       rde-services
+                       (if (rde-config-integrate-he-in-os? config)
+                           (list (service guix-home-service-type
+                                          ;; TODO: [Andrew Tropin, 2024-05-27]
+                                          ;; Temporary fix, remove it, when
+                                          ;; https://issues.guix.gnu.org/71111 is
+                                          ;; merged
+                                          `(,(list
+                                              rde-user-name
+                                              (get-home-environment config)))))
+                           '())
+                       (list (service sudoers-service-type))
+                       (if rde-user-name
+                           (list (service rde-account-service-type rde-user))
+                           '())))
+            (sudoers-file #f)
+            (name-service-switch rde-name-service-switch))))
+    ;; Only apply transformations on thunked fields here.
+    (operating-system
+      (inherit computed-os)
+      (essential-services
+       (modify-services (operating-system-essential-services computed-os)
+         (shepherd-root-service-type
+          this-config =>
+          (shepherd-configuration
+           (inherit this-config)
+           (shepherd (get-value 'shepherd config)))))))))
