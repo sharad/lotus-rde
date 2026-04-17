9cbfd66055de4771ad45c38f8fc858bda05c062f
Sharad Pratap <110339+sharad@users.noreply.github.com>
2026-04-17 10:51:49 +0530

    correction

--- examples/src/rde-configs/hosts/gx2-guix-vmware.scm
+++ examples/src/rde-configs/hosts/gx2-guix-vmware.scm
@@ -9,41 +9,42 @@
 
 
 
-;; (define-public %gx2-guix-vmware-features (define-lotus-machine-features "gx2-guix-vmware"
-;;                                            #:disk-serial-if-system "vmware"
-;;                                            #:disk-serial-if-home "vmware"
-;;                                            #:fs-boot-efi-partition (uuid "4D78-999F" 'fat32)
-;;                                            #:kernel-arguments (append (list "usbcore.autosuspend=-1"
-;;                                                                        "libata.force=2:disable"
-;;                                                                        "libata.noacpi=1"
-;;                                                                        "libata.ignore_hpa=1"
-;;                                                                        "--verbose"
-;;                                                                        "nosplash"
-;;                                                                        "debug"))
-;;                                            ;; (if (and (pair? %lotus-swap-devices)
-;;                                            ;;          (> (length %lotus-swap-devices) 0))
-;;                                            ;;     (list (string-append "resume="
-;;                                            ;;                          (swap-space-target (car %lotus-swap-devices))))
-;;                                            ;;     '())
-;;                                            #:initrd (lambda (file-systems . rest)
-;;                                                       (apply base-initrd file-systems
-;;                                                              #:extra-modules '("virtio.ko"
-;;                                                                                "virtio_balloon.ko"
-;;                                                                                "virtio_ring.ko"
-;;                                                                                "virtio_blk.ko"
-;;                                                                                "virtio_pci.ko"
-;;                                                                                ;; https://issues.guix.gnu.org/31887
-;;                                                                                "mptbase.ko"
-;;                                                                                "mptscsih.ko"
-;;                                                                                "mptspi.ko"
-;;                                                                                "virtio_net.ko")
-;;                                                              rest))
-;;                                            #:custom-services (feature-custom-services
-;;                                                               #:feature-name-prefix 'gx2-guix-vmware-extra
-;;                                                               #:system-services
-;;                                                               (list))))
+(define-public %gx2-guix-vmware-features (define-lotus-machine-features "gx2-guix-vmware"
+                                           #:disk-serial-if-system "vmware"
+                                           #:disk-serial-if-home "vmware"
+                                           #:fs-boot-efi-partition (uuid "4D78-999F" 'fat32)
+                                           #:kernel-arguments (append (list "usbcore.autosuspend=-1"
+                                                                       "libata.force=2:disable"
+                                                                       "libata.noacpi=1"
+                                                                       "libata.ignore_hpa=1"
+                                                                       "--verbose"
+                                                                       "nosplash"
+                                                                       "debug"))
+                                           ;; (if (and (pair? %lotus-swap-devices)
+                                           ;;          (> (length %lotus-swap-devices) 0))
+                                           ;;     (list (string-append "resume="
+                                           ;;                          (swap-space-target (car %lotus-swap-devices))))
+                                           ;;     '())
+                                           #:initrd (lambda (file-systems . rest)
+                                                      (apply base-initrd file-systems
+                                                             #:extra-modules '("virtio.ko"
+                                                                               "virtio_balloon.ko"
+                                                                               "virtio_ring.ko"
+                                                                               "virtio_blk.ko"
+                                                                               "virtio_pci.ko"
+                                                                               ;; https://issues.guix.gnu.org/31887
+                                                                               "mptbase.ko"
+                                                                               "mptscsih.ko"
+                                                                               "mptspi.ko"
+                                                                               "virtio_net.ko")
+                                                             rest))
+                                           #:custom-services (feature-custom-services
+                                                              #:feature-name-prefix 'gx2-guix-vmware-extra
+                                                              #:system-services
+                                                              (list))))
 
 
-(define-public %gx2-guix-vmware-features (list))
+(display %gx2-guix-vmware-features)
+(newline)
 
 
