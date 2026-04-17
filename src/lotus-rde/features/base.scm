93ce2b45f6454b0ea7b2bcd59ac02bf18b34206c
Sharad Pratap <110339+sharad@users.noreply.github.com>
2026-04-17 15:58:00 +0530

    correction

--- src/lotus-rde/features/base.scm
+++ src/lotus-rde/features/base.scm
@@ -1,6 +1,7 @@
 (define-module (lotus-rde features base)
   #:use-module (ice-9 match)
   #:use-module (srfi srfi-11)
+  #:use-module (srfi srfi-64)
   #:use-module (gnu bootloader)
   #:use-module (gnu bootloader grub)
   #:use-module (gnu system linux-initrd)
