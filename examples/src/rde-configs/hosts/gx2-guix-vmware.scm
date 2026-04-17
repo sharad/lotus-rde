b184a09656a1588339eeb80e51335a5eecaad3ac
Sharad Pratap <110339+sharad@users.noreply.github.com>
2026-04-17 15:52:45 +0530

    correction

--- examples/src/rde-configs/hosts/gx2-guix-vmware.scm
+++ examples/src/rde-configs/hosts/gx2-guix-vmware.scm
@@ -27,19 +27,19 @@
                                            ;;     (list (string-append "resume="
                                            ;;                          (swap-space-target (car %lotus-swap-devices))))
                                            ;;     '())
-                                           #:initrd (lambda (file-systems . rest)
-                                                      (apply base-initrd file-systems
-                                                             #:extra-modules '("virtio.ko"
-                                                                               "virtio_balloon.ko"
-                                                                               "virtio_ring.ko"
-                                                                               "virtio_blk.ko"
-                                                                               "virtio_pci.ko"
-                                                                               ;; https://issues.guix.gnu.org/31887
-                                                                               "mptbase.ko"
-                                                                               "mptscsih.ko"
-                                                                               "mptspi.ko"
-                                                                               "virtio_net.ko")
-                                                             rest))
+                                           ;; #:initrd (lambda (file-systems . rest)
+                                           ;;            (apply base-initrd file-systems
+                                           ;;                   #:extra-modules '("virtio.ko"
+                                           ;;                                     "virtio_balloon.ko"
+                                           ;;                                     "virtio_ring.ko"
+                                           ;;                                     "virtio_blk.ko"
+                                           ;;                                     "virtio_pci.ko"
+                                           ;;                                     ;; https://issues.guix.gnu.org/31887
+                                           ;;                                     "mptbase.ko"
+                                           ;;                                     "mptscsih.ko"
+                                           ;;                                     "mptspi.ko"
+                                           ;;                                     "virtio_net.ko")
+                                           ;;                   rest))
                                            #:custom-services (feature-custom-services
                                                               #:feature-name-prefix 'gx2-guix-vmware-extra
                                                               #:system-services (list))))
--- src/lotus-rde/features/base.scm
+++ src/lotus-rde/features/base.scm
@@ -685,6 +685,26 @@
                                                                  #:fs-boot-efi-partition fs-boot-efi-partition))
                      ((home-devices home-fs) (lotus-devfs-home ;; #:fs-root rootfs
                                                                #:disk-serial-id disk-serial-id-home)))
+
+
+
+          (assert (list? sys-devices) "sys-devices not list")
+          (assert (list? home-devices) "home-devices not list")
+          (assert (list? sys-fs) "sys-fs not list")
+          (assert (list? home-fs) "home-fs not list")
+          (for-each (lambda (x)
+                      (assert x "sys-device contains #f"))
+                    sys-devices)
+          (for-each (lambda (x)
+                      (assert x "home-device contains #f"))
+                    home-devices)
+          (for-each (lambda (x)
+                      (assert x "sys-fs contains #f"))
+                    sys-fs)
+          (for-each (lambda (x)
+                      (assert x "home-fs contains #f"))
+                    home-fs)
+
           (feature-file-systems #:mapped-devices (append sys-devices home-devices)
                                 #:file-systems (append sys-fs home-fs)
                                 ;; #:swap-devices (list (lotus-devfs-swap))
