276a7b0ff6d91ab36f7bab7bd76460dd364f5d98
Sharad Pratap <110339+sharad@users.noreply.github.com>
2026-04-17 14:20:21 +0530

    correction

--- src/lotus-rde/features/base.scm
+++ src/lotus-rde/features/base.scm
@@ -681,15 +681,15 @@
         ;; Allows to declare specific bootloader configuration,
         ;; grub-efi-bootloader used by default
         ;; (feature-bootloader)
-        ;; (let-values (((rootfs sys-devices sys-fs) (lotus-devfs-system
-        ;;                                            #:disk-serial-id disk-serial-if-system
-        ;;                                            #:fs-boot-efi-partition fs-boot-efi-partition))
-        ;;              ((home-devices home-fs) (lotus-devfs-home
-        ;;                                       #:disk-serial-id disk-serial-if-home)))
-        ;;   (feature-file-systems #:mapped-devices (append sys-devices home-devices)
-        ;;                         #:file-systems (append sys-fs home-fs)
-        ;;                         ;; #:swap-devices (list (lotus-devfs-swap))
-        ;;                         #:user-pam-file-systems '()))
+        (let-values (((_ sys-devices sys-fs) (lotus-devfs-system
+                                              #:disk-serial-id disk-serial-if-system
+                                              #:fs-boot-efi-partition fs-boot-efi-partition))
+                     ((home-devices home-fs) (lotus-devfs-home
+                                              #:disk-serial-id disk-serial-if-home)))
+          (feature-file-systems #:mapped-devices (append sys-devices home-devices)
+                                #:file-systems (append sys-fs home-fs)
+                                ;; #:swap-devices (list (lotus-devfs-swap))
+                                #:user-pam-file-systems '()))
 
 
 
