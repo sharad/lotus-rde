754afacaa6e387065bbd35e0b6de26a04fbf43a9
Sharad Pratap <110339+sharad@users.noreply.github.com>
2026-04-19 00:40:20 +0530

    correction

--- src/lotus-rde/features/system.scm
+++ src/lotus-rde/features/system.scm
@@ -24,15 +24,15 @@
 
 
 (define* (feature-lotus-machine hostname
-           #:key
-           (disk-serial-id-system "aaa")
-           (disk-serial-id-home "aaa")
-           (fs-boot-efi-partition (uuid "0000-0000" 'fat32))
-           (kernel linux-libre)
-           (firmware '())
-           (kernel-arguments '())
-           (initrd base-initrd)
-           (custom-services #f))
+                                #:key
+                                (disk-serial-id-system "aaa")
+                                (disk-serial-id-home "aaa")
+                                (fs-boot-efi-partition (uuid "0000-0000" 'fat32))
+                                (kernel linux-libre)
+                                (firmware '())
+                                (kernel-arguments '())
+                                (initrd base-initrd)
+                                (custom-services #f))
   (list (feature-host-info #:host-name hostname
                            ;; #:locale    (operating-system-locale bare-bone-os)
                            ;; ls `guix build tzdata`/share/zoneinfo
@@ -41,41 +41,16 @@
                         #:initrd initrd
                         #:firmware firmware
                         #:kernel-arguments kernel-arguments)
-        ;; (feature-bootloader #:bootloader-configuration (bootloader-configuration (bootloader grub-bootloader)
-        ;;                                                                          (targets    '())))
+        (feature-bootloader #:bootloader-configuration (bootloader-configuration (bootloader grub-bootloader)
+                                                                                 (targets    '())))
                                                                                  ;; (keyboard-layout %lotus-keyboard-layout)
                                                                                  ;; (menu-entries    %lotus-grub-ubuntu-menuentries)
         ;; Allows to declare specific bootloader configuration,
         ;; grub-efi-bootloader used by default
         ;; (feature-bootloader)
-
         (feature-mapped-file-systems #:disk-serial-id-system disk-serial-id-system
                                      #:disk-serial-id-home disk-serial-id-home
                                      #:fs-boot-efi-partition fs-boot-efi-partition)
-        ;; (let-values (((_ sys-devices sys-fs) (lotus-devfs-system #:disk-serial-id disk-serial-id-system
-        ;;                                                          #:fs-boot-efi-partition fs-boot-efi-partition))
-        ;;              ((home-devices home-fs) (lotus-devfs-home ;; #:fs-root rootfs
-        ;;                                                        #:disk-serial-id disk-serial-id-home)))
-        ;;   ;; (assert (list? sys-devices) "sys-devices not list")
-        ;;   ;; (assert (list? home-devices) "home-devices not list")
-        ;;   ;; (assert (list? sys-fs) "sys-fs not list")
-        ;;   ;; (assert (list? home-fs) "home-fs not list")
-        ;;   ;; (for-each (lambda (x)
-        ;;   ;;             (assert x "sys-device contains #f"))
-        ;;   ;;           sys-devices)
-        ;;   ;; (for-each (lambda (x)
-        ;;   ;;             (assert x "home-device contains #f"))
-        ;;   ;;           home-devices)
-        ;;   ;; (for-each (lambda (x)
-        ;;   ;;             (assert x "sys-fs contains #f"))
-        ;;   ;;           sys-fs)
-        ;;   ;; (for-each (lambda (x)
-        ;;   ;;             (assert x "home-fs contains #f"))
-        ;;   ;;           home-fs)
-        ;;   (feature-file-systems #:mapped-devices (append sys-devices home-devices)
-        ;;                         #:file-systems (append sys-fs home-fs)
-        ;;                         ;; #:swap-devices (list (lotus-devfs-swap))
-        ;;                         #:user-pam-file-systems '()))
         (feature-base-services)
         ;; (feature-desktop-services)
 
