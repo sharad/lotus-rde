5cbad17a6695ad3f446764bb443c16f021e4ed42
Sharad Pratap <110339+sharad@users.noreply.github.com>
2026-04-23 14:32:51 +0530

    correction

--- examples/env/guix/rde-configs/env/guix/channels-ci-latest-guix.scm
+++ examples/env/guix/rde-configs/env/guix/channels-ci-latest-guix.scm
@@ -21,6 +21,6 @@
    ;;   "257cebd587b66e4d865b3537a9a88cccd7107c95"
    ;;   (openpgp-fingerprint
    ;;    "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0")))
-   rde:core-channels))
+   rde:core-channelscore-channelscore-channels))
 
 core-channels-with-local-rde-and-latest-guix
