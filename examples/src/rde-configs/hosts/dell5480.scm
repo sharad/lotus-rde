b1aab8abae4f5c3d891ecccd6d17682cf2f34132
Sharad Pratap <110339+sharad@users.noreply.github.com>
2026-04-24 15:15:27 +0530

    correction

--- examples/src/rde-configs/hosts/dell5480.scm
+++ examples/src/rde-configs/hosts/dell5480.scm
@@ -12,41 +12,38 @@
 ;; TODO: Switch from UUIDs to partition labels For better
 ;; reproducibilty and easier setup.  Grub doesn't support luks2 yet.
 
-(define dell5480-mapped-devices
-  (list (mapped-device
-          (source (uuid "6243841f-4171-43dd-8e0b-93bddd56daaa"))
-          (target "enc")
-          (type luks-device-mapping))))
-
-(define dell5480-file-systems
-  (append ;; btrfs-subvolumes
-          (list
-           ;; persist all system data to data
-           (file-system
-             (device "/data/system/var/lib")
-             (type "none")
-             (mount-point "/var/lib")
-             (flags '(bind-mount))
-             ;; (options "bind")
-             )
-           (file-system
-             (mount-point "/boot/efi")
-             (type "vfat")
-             (device (uuid "97DB-35DC"
-                           'fat32))))))
-
-(define-public %dell5480-features
-  (list (feature-host-info #:host-name "dell5480"
-                           ;; ls `guix build tzdata`/share/zoneinfo
-                           #:timezone "Asia/Kolkata")
-        ;; Allows to declare specific bootloader configuration,
-        ;; grub-efi-bootloader used by default
-        ;; (feature-bootloader)
-        (feature-file-systems #:mapped-devices dell5480-mapped-devices
-                              #:file-systems dell5480-file-systems)
-        (feature-kanshi #:extra-config `((profile laptop
-                                                  ((output eDP-1 enable)))
-                                         (profile docked
-                                                  ((output eDP-1 enable)
-                                                   (output DP-2 scale 2)))))
-        (feature-hidpi)))
+(define-public %dell5480-features (feature-lotus-machine "dell5480"
+                                                         #:kernel linux
+                                                         #:initrd microcode-initrd
+                                                         #:firmware (list linux-firmware)
+                                                         #:disk-serial-id-system "z7mp9s"
+                                                         #:disk-serial-id-home "z7mp9s"
+                                                         #:fs-boot-efi-partition (uuid "7528-DF8A" 'fat32)
+                                                         #:kernel-arguments (list "usbcore.autosuspend=-1"
+                                                                                  "libata.force=2:disable"
+                                                                                  "libata.noacpi=1"
+                                                                                  "libata.ignore_hpa=1"
+                                                                                  "--verbose"
+                                                                                  "nosplash"
+                                                                                  "debug")
+                                                         ;; (if (and (pair? %lotus-swap-devices)
+                                                         ;;          (> (length %lotus-swap-devices) 0))
+                                                         ;;     (list (string-append "resume="
+                                                         ;;                          (swap-space-target (car %lotus-swap-devices))))
+                                                         ;;     '())
+                                                         ;; #:initrd (lambda (file-systems . rest)
+                                                         ;;            (apply base-initrd file-systems
+                                                         ;;                   #:extra-modules '("virtio.ko"
+                                                         ;;                                     "virtio_balloon.ko"
+                                                         ;;                                     "virtio_ring.ko"
+                                                         ;;                                     "virtio_blk.ko"
+                                                         ;;                                     "virtio_pci.ko"
+                                                         ;;                                     ;; https://issues.guix.gnu.org/31887
+                                                         ;;                                     "mptbase.ko"
+                                                         ;;                                     "mptscsih.ko"
+                                                         ;;                                     "mptspi.ko"
+                                                         ;;                                     "virtio_net.ko")
+                                                         ;;                   rest))
+                                                         #:custom-services (feature-custom-services
+                                                                            #:feature-name-prefix 'dell5480-extra
+                                                                            #:system-services (list))))
