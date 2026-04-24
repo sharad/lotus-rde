b3f1fee62be1ec4017137f75f28d90cf1ea8bd15
Sharad Pratap <110339+sharad@users.noreply.github.com>
2026-04-24 20:01:53 +0530

    correction

--- examples/env/guix/rde-configs/env/guix/channels.scm
+++ examples/env/guix/rde-configs/env/guix/channels.scm
@@ -16,7 +16,7 @@
           (name 'lotus-rde)
           (url "https://github.com/sharad/lotus-rde.git")
           (branch "master")
-          (commit "c0388a9688b5f6d60be54d2a6b0a2996a9aa9758"))
+          (commit "90039bd4fcb41500a998678e5f71a4f76ca29a3e"))
         ;; (introduction
         ;; (make-channel-introduction
         ;; "257cebd587b66e4d865b3537a9a88cccd7107c95"
