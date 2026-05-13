c1d7c1494a49c7c91eb698c8da9d91307d50f182
Sharad Pratap <110339+sharad@users.noreply.github.com>
2026-05-13 11:43:15 +0530

    correction

--- examples/src/rde-configs/hosts/dell5480.scm
+++ examples/src/rde-configs/hosts/dell5480.scm
@@ -26,24 +26,12 @@
                                                                                "--verbose"
                                                                                "nosplash"
                                                                                "debug")
-                                                      ;; (if (and (pair? %lotus-swap-devices)
-                                                      ;;          (> (length %lotus-swap-devices) 0))
-                                                      ;;     (list (string-append "resume="
-                                                      ;;                          (swap-space-target (car %lotus-swap-devices))))
-                                                      ;;     '())
-                                                      ;; #:initrd (lambda (file-systems . rest)
-                                                      ;;            (apply base-initrd file-systems
-                                                      ;;                   #:extra-modules '("virtio.ko"
-                                                      ;;                                     "virtio_balloon.ko"
-                                                      ;;                                     "virtio_ring.ko"
-                                                      ;;                                     "virtio_blk.ko"
-                                                      ;;                                     "virtio_pci.ko"
-                                                      ;;                                     ;; https://issues.guix.gnu.org/31887
-                                                      ;;                                     "mptbase.ko"
-                                                      ;;                                     "mptscsih.ko"
-                                                      ;;                                     "mptspi.ko"
-                                                      ;;                                     "virtio_net.ko")
-                                                      ;;                   rest))
+                                                      #:volume-mappings '( ("z7mp9s"
+                                                                            ((("vg01" "lv01"))
+                                                                             (("vg02" "lv01"))
+                                                                             (("vg02" "lv02")))
+                                                                            #:prefix "vds"
+                                                                            #:seq 0))
                                                       #:custom-services (feature-custom-services
                                                                          #:feature-name-prefix 'dell5480-extra
                                                                          #:system-services (list))))
