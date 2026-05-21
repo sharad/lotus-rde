73f59ada40f16c7d3c2d006b793302976f939ee8
Sharad Pratap <110339+sharad@users.noreply.github.com>
2026-05-21 15:30:23 +0530

    correction

--- src/lotus-rde/features/misc.scm
+++ src/lotus-rde/features/misc.scm
@@ -79,8 +79,8 @@
   #:use-module (gnu packages messaging)
   #:use-module (gnu packages networking)
   #:use-module (gnu packages linux)
-  #:use-module (shepherd service)
-  ;; #:use-module (rde features)
+  ;; #:use-module (shepherd service)
+  #:use-module (rde features)
   ;; #:use-module (rde features predicates)
 
   #:use-module (gnu home services)
